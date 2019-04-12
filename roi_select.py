1b7ee9ad4c7853a69a1a6ac647b5d323e1412370
Karlo Koscevic <karlo.koscevic@gmail.com>
2019-04-12 17:29:11 +0200

    Implement basic selector

--- roi_select.py
+++ roi_select.py
@@ -2,59 +2,127 @@ import click
 import cv2
 import numpy as np
 
-VERTEX_RADIUS = 10
+VERTEX_RADIUS = 5
 
 class RoiSelector:
-	def __init__(self, img, n_vertices):
-		self.img = img
-		self.working_img = img.copy()
-		self.n_vertices = n_vertices
+	def __init__(self, img, max_vertices):
+		self.main_window = 'Selector'
+		self.crop_window = 'Cropped Region'
+		self.img = img.copy()
+		self.max_vertices = max_vertices
 		self.vertices = []
+		self.n_vertices = 0
+		self.move_vertex = None
+		self.additional_vertices = 0
 
-	def close_to(self):
-		pass
+	@staticmethod
+	def close_to(v1, v2):
+		return np.linalg.norm([v1[0] - v2[0], v1[1] - v2[1]]) < VERTEX_RADIUS
 
 	def on_click(self, event, x, y, flags, params):
-		if event == cv2.EVENT_LBUTTONDOWN:
-			for vertex in self.vertices:
-				print(vertex)
-				if np.linalg.norm([vertex[0] - x, vertex[1] - y]) < VERTEX_RADIUS:
-					print('close')
-					print(np.linalg.norm([vertex[0] - x, vertex[1] - y]))
-
-			if len(self.vertices) < self.n_vertices:
+		if self.move_vertex is not None:
+			self.vertices[self.move_vertex] = [x, y]
+		if event == cv2.EVENT_LBUTTONDOWN and flags & cv2.EVENT_FLAG_CTRLKEY:
+			if self.move_vertex is None:
+				for idx, vertex in enumerate(self.vertices):
+					if self.close_to([x, y], vertex):
+						self.move_vertex = idx
+			else:
+				self.vertices[self.move_vertex] = [x, y]
+				self.move_vertex = None
+		elif event == cv2.EVENT_LBUTTONDOWN and self.move_vertex is None:
+			if self.n_vertices < self.max_vertices + self.additional_vertices:
 				self.vertices += [[x, y]]
-				cv2.circle(self.working_img, (x, y), VERTEX_RADIUS, (0,0,255), -1)
-				#print('({}, {})'.format(x, y))
+				self.n_vertices += 1
+
 
 	def plot(self):
-		cv2.namedWindow('display', cv2.WINDOW_NORMAL)
-		cv2.setWindowProperty('display',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-		cv2.setMouseCallback('display', self.on_click)	
+		working_img = self.img.copy()
+
+		for idx, vertex in enumerate(self.vertices):
+			if idx != self.move_vertex:
+				cv2.circle(working_img, (vertex[0], vertex[1]), VERTEX_RADIUS, (0,0,255), -1)
+			else:
+				cv2.circle(working_img, (vertex[0], vertex[1]), VERTEX_RADIUS, (200,0,200), -1)
+
+		if self.n_vertices > 1:
+			vertices = self.vertices.copy()
+			vertices += [self.vertices[0]]
+			for v1, v2 in zip(vertices[:-2], vertices[1:-1]):
+				cv2.line(working_img, (v1[0], v1[1]), (v2[0], v2[1]),(255,0,0),2)
+			if self.n_vertices == self.max_vertices + self.additional_vertices:
+				v1, v2 = self.vertices[-1], self.vertices[0]
+				cv2.line(working_img, (v1[0], v1[1]), (v2[0], v2[1]),(255,0,0),2)
+
+		cv2.putText(working_img, '{}/{}'.format(len(self.vertices), self.max_vertices + self.additional_vertices), (10,100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,0,0), 3)
+		cv2.imshow(self.main_window, working_img)
+
+	def get_vertices(self):
+		return self.vertices
+
+	def get_mask(self):
+		mask = np.zeros_like(self.img)
+		ignore_mask_color = (255,) * self.img.shape[2]
+		cv2.fillPoly(mask, np.array([self.vertices], dtype=np.int32), ignore_mask_color)
+
+		return mask
+
+	def run(self):
+		cv2.namedWindow(self.main_window, cv2.WINDOW_NORMAL)
+		cv2.setWindowProperty(self.main_window,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+		cv2.setMouseCallback(self.main_window, self.on_click)	
 
 		while True:
-			plot_img = self.working_img.copy()
-			cv2.putText(plot_img, '{}/{}'.format(len(self.vertices), self.n_vertices), (10,100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,0,0), 5)
-
-			cv2.imshow('display', plot_img)
+			self.plot()
 			key = cv2.waitKey(1) & 0xFF
 
 			if key == ord('r'):
 				self.vertices = []
-				self.working_img = self.img.copy()
+				self.n_vertices = 0
+				self.move_vertex = None
+				self.additional_vertices = 0
+				cv2.destroyWindow(self.crop_window)
+
+			if key == ord('a'):
+				self.additional_vertices += 1
+				cv2.destroyWindow(self.crop_window)
+
+			if key == ord('s'):
+				if self.additional_vertices > 0:
+					if self.n_vertices == self.max_vertices + self.additional_vertices:
+						self.vertices = self.vertices[:-1]
+						self.n_vertices -= 1
+
+					self.additional_vertices -= 1
+					cv2.destroyWindow(self.crop_window)
+
+			if key == ord('m'):
+				if self.n_vertices == self.max_vertices + self.additional_vertices:
+					cv2.destroyAllWindows()
+					return self.get_mask()
+
+			if key == ord('d'):
+				cv2.destroyWindow(self.crop_window)				
+
+			if key == ord('c'):
+				if self.n_vertices == self.max_vertices + self.additional_vertices:
+					mask = self.get_mask()
+					masked_image = cv2.bitwise_and(self.img, mask)
+					cv2.namedWindow(self.crop_window, cv2.WINDOW_NORMAL)
+					cv2.setWindowProperty(self.crop_window,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+					cv2.imshow(self.crop_window, masked_image)
 
 			if key == ord('q'):
 				cv2.destroyAllWindows()
 				break
 
 @click.command()
-@click.option('-n', default=2)
+@click.option('-n', default=3, type=click.IntRange(3))
 def main(n):
 	img = cv2.imread('0001.png', -1).astype(np.float32)
-	img = img / img.max()
+	img = (img / img.max() * 255).astype(np.uint8)
 	
-	RoiSelector(img, n).plot()
+	RoiSelector(img, n).run()
 
-	
 if __name__ == '__main__':
 	main()
